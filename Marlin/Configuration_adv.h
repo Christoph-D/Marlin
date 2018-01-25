c50d1b7764b071126f0e773fbec10a89ebb7abc0
Bart <bartvanrijs@gmail.com>
2018-01-25 09:14:50 +0100

    Increased thermal protection period & enabled LIN_ADVANCE
    - The stock hotend sometimes has a hard time maintaning the set temp resulting in thermal runaways. By increasing the thermal protection period, there is more time for the hotend to recover.
    - LIN_advance enabled by default and set to 0. To use LIN-Advance set the K-factor in the start script of the slicer.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -75,8 +75,8 @@
  * THERMAL_PROTECTION_HYSTERESIS and/or THERMAL_PROTECTION_PERIOD
  */
 #if ENABLED(THERMAL_PROTECTION_HOTENDS)
-  #define THERMAL_PROTECTION_PERIOD 40        // Seconds
-  #define THERMAL_PROTECTION_HYSTERESIS 4     // Degrees Celsius
+  #define THERMAL_PROTECTION_PERIOD 60        // Seconds
+  #define THERMAL_PROTECTION_HYSTERESIS 8     // Degrees Celsius
 
   /**
    * Whenever an M104, M109, or M303 increases the target temperature, the
@@ -835,9 +835,9 @@
  * See http://marlinfw.org/docs/features/lin_advance.html for full instructions.
  * Mention @Sebastianv650 on GitHub to alert the author of any issues.
  */
-//#define LIN_ADVANCE
+#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
-  #define LIN_ADVANCE_K 0.22  // Unit: mm compression per 1mm/s extruder speed
+  #define LIN_ADVANCE_K 0  // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG          // If enabled, this will generate debug information output over USB.
 #endif
 
